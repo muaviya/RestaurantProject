034d3309a9888fbaf997bee1bae06d7362595545
Muaviya <ismaisan@mail.ru>
2016-05-19 12:06:27 +0300

    add 2 framework SDWEBImage and SWRevealController. Create the extension menu and setting navigation item.

---
+++ RestaurantProject1/RestaurantProject1-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "UIImageView+WebCache.h"
+#import "SWRevealViewController.h"
+#import <CommonCrypto/CommonCrypto.h>
---
+++ RestaurantProject1/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  RestaurantProject1
+//
+//  Created by Muaviya on 19.05.16.
+//  Copyright © 2016 DGUNH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+    }
+
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.title = "Главная"
+        
+        let nav = self.navigationController?.navigationBar
+        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.greenColor()]
+        self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
+
+    }
+    
+}
+
