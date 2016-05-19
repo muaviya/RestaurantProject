0fdd55847fd6afd74cb843c2389add5febf43ffe
Muaviya <ismaisan@mail.ru>
2016-05-19 13:21:41 +0300

    fix presentViewController

--- RestaurantProject1/ViewController.swift
+++ RestaurantProject1/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
-
+    
+    let reach = Reachability()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,8 +22,6 @@ class ViewController: UIViewController {
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-        
-        alertInternet()
 
     }
 
@@ -33,25 +33,31 @@ class ViewController: UIViewController {
         let nav = self.navigationController?.navigationBar
         nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.greenColor()]
         self.navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
-
-    }
     
-    func alertInternet(){
         
-        let rechability = Reachability()
-        if !rechability.isConnectedToNetwork() {
+        if !reach.isConnectedToNetwork() {
             
-            let alertController = UIAlertController(title: "У вас нет подключения к интернету", message: "Проверьте подключение", preferredStyle: .Alert)
-            
-            let alertAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
-            
-            alertController.addAction(alertAction)
-            
-            self.presentViewController(alertController, animated: true, completion: nil)
+            alertIsNotConnectNetwork()
             
         }
         
     }
+    
+    func alertIsNotConnectNetwork() {
+        
+        let alertController = UIAlertController(title: "У вас нет подключения к интернету", message: "Проверьте подключение", preferredStyle: .Alert)
+        
+        let alertAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alertController.addAction(alertAction)
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.presentViewController(alertController, animated: true, completion: nil)
+        })
+        
+
+    }
+    
     
     
 }
