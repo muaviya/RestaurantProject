a755d426126448b85c5123b06ca293dd22fac9f7
Muaviya <ismaisan@mail.ru>
2016-05-26 16:47:42 +0300

    add collection view in liked vc. and connect collection view with array food(test)

---
+++ RestaurantProject1/LikedCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LikedCollectionViewCell.swift
+//  RestaurantProject1
+//
+//  Created by Muaviya on 26.05.16.
+//  Copyright © 2016 DGUNH. All rights reserved.
+//
+
+import UIKit
+
+class LikedCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelString: UILabel!
+    @IBOutlet weak var imageFoodMainVC: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
--- RestaurantProject1/LikedVC.swift
+++ RestaurantProject1/LikedVC.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class LikedVC: UIViewController {
+class LikedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var foodModel : [Food] = [Food]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.addModeltoArrayFood()
+
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.title = "Избранное"
     }
     
+    func addModeltoArrayFood() {
+        
+        foodModel = [Food(name: "Пюре", price: 100), Food(name: "Пицца", price: 180)]
+    }
+
+    
+    // обработка нажатия
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let vc : AboutDishVC = self.storyboard?.instantiateViewControllerWithIdentifier("AboutDishVC") as! AboutDishVC
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // UICollectionViewDataSource
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return foodModel.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath) as! LikedCollectionViewCell
+        
+        cell.labelString.text = foodModel[indexPath.row].name
+        cell.priceLabel.text = String(foodModel[indexPath.row].price) + "р"
+        
+        return cell
+        
+    }
+
 }
